a370f1ef4e61917ccdf77bdb06f65f79862e01ee
Ognjen Marinkovic <pingvincar@gmail.com>
2025-02-03 22:46:00 +0100

    add GameActionsService with Start method, remove CLOSED game state

--- models/game.go
+++ models/game.go
@@ -4,7 +4,6 @@ type GameState string
 
 const (
 	OPEN    GameState = "OPEN"
-	CLOSED  GameState = "CLOSED"
 	RUNNING GameState = "RUNNING"
 	DONE    GameState = "DONE"
 )
---
+++ services/gameactions.go
@@ -0,0 +1,68 @@
+package services
+
+import (
+	"errors"
+	"mognjen/gossassins/apierrors"
+	"mognjen/gossassins/models"
+	"mognjen/gossassins/repos"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/supabase-community/supabase-go"
+)
+
+type GameActionsService struct {
+	gameRepo *repos.GameRepo
+	client   *supabase.Client
+}
+
+func (s *GameActionsService) Start(gameId int) apierrors.StatusError {
+	game, err := s.gameRepo.GetById(gameId)
+	if err != nil {
+		return err
+	}
+
+	if game.State == "STARTED" {
+		return apierrors.NewStatusError(http.StatusBadRequest, errors.New("Game already started"))
+	}
+
+	err = s.assignTargets(game)
+	if err != nil {
+		return err
+	}
+
+	err = s.setGameStatusToRunning(err, gameId, game)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *GameActionsService) setGameStatusToRunning(err apierrors.StatusError, gameId int, game *models.Game) apierrors.StatusError {
+	err = s.gameRepo.Patch(gameId, &models.Game{
+		Id:        game.Id,
+		Name:      game.Name,
+		CreatedBy: game.CreatedBy,
+		State:     models.RUNNING,
+	})
+
+	if err != nil {
+		return apierrors.NewStatusError(
+			http.StatusInternalServerError,
+			err,
+		)
+	}
+	return nil
+}
+
+func (s *GameActionsService) assignTargets(game *models.Game) apierrors.StatusError {
+	errMsg := s.client.Rpc("assign_kill_codes_and_targets", "", gin.H{"p_game_id": game.Id})
+	if errMsg != "" {
+		return apierrors.NewStatusError(
+			http.StatusInternalServerError,
+			errors.New(errMsg),
+		)
+	}
+	return nil
+}
